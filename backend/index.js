5ac163002fa26840aa6801e566fed5512ded7fab
Hazael Vite <14224891+HazaelVite@users.noreply.github.com>
2022-10-13 13:57:40 -0500

    Routing del usuario

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import usuarioRoutes from './routes/usuarioRoutes.js';
 
 const app = express();
 
@@ -8,8 +9,10 @@ dotenv.config();
 
 connectDB();
 
-const PORT = process.env.PORT || 4000;
+// Routing
+app.use('/api/usuarios', usuarioRoutes);
 
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto 4000`);
 });
